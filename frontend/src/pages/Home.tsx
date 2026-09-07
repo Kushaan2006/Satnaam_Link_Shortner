8550d939111cf6b4589f9b1485ec75d8a7d630f5
Kushaan2006 <kushaan731@gmail.com>
2026-09-07 14:06:22 -0400

    Progress: the homepage can now create urls and show output. Also, added management of user state.,

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,29 +1,65 @@
-import { useState } from "react";
+import { use, useEffect, useState } from "react";
 import AuthModal from "../components/AuthModal";
 import LogoutButton from "../components/LogoutButton";
 import { useAuth } from "../../hooks/useAuth";
 import { api } from "../api/api";
+import axios from "axios";
+import { redirect } from "react-router-dom";
 
 export default function Home() {
   const { user } = useAuth();
   const [url, setUrl] = useState("");
   const [mode, setMode] = useState<"auto" | "custom">("auto");
   const [custom, setCustom] = useState("");
+  const [outputUrl, setOutputUrl] = useState("");
 
+  const [showModal, setShowModal] = useState(false);
+  const [pendingCreateReq, setPendingCreateReq] = useState(false);
   const [message, setMessage] = useState("");
-  const handleFormSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
-    e.preventDefault();
+
+  useEffect(() => {
+    if (user && pendingCreateReq) {
+      setShowModal(false);
+      setPendingCreateReq(false);
+      createReq();
+    }
+  });
+
+  const createReq = async () => {
     try {
       //Send Req.
-      const response = api.post;
-      //If not logged in first login/signup pop up
+      const response = await api.post("/urls/", { url, custom });
+      console.log("Success created URL: ", response.data);
+      setOutputUrl(
+        `${import.meta.env.VITE_BACKEND_URL}${response.data.shortUrl}`,
+      );
+    } catch (error) {
+      console.error(error);
       //If already exists or error setMsg to it
-    } catch (error) {}
+      if (axios.isAxiosError(error)) {
+        setMessage(error.response?.data?.message || "Something went wrong");
+      } else {
+        setMessage("Something went wrong");
+      }
+    }
+  };
+
+  const handleFormSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    //If not logged in first login/signup pop up
+
+    if (!user) {
+      setShowModal(true);
+      setPendingCreateReq(true);
+      return;
+    }
+
+    createReq();
   };
 
   return (
     <>
-      {!user && <AuthModal />}
+      {showModal && <AuthModal />}
       <LogoutButton />
       <main>
         <h1>Shorten your link</h1>
@@ -47,6 +83,15 @@ export default function Home() {
               value={custom}
               onChange={(e) => setCustom(e.target.value)}
               placeholder="Enter the custom url"
+            />
+          )}
+          {outputUrl && (
+            <input
+              readOnly
+              value={outputUrl}
+              onClick={() => {
+                //navigate to url
+              }}
             />
           )}
           {message && <p>{message}</p>}
