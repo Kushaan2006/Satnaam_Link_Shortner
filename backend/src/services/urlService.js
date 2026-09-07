95ab2665b16827ec3f0c299adef4c144713b1ffc
Kushaan2006 <kushaan731@gmail.com>
2026-09-07 13:33:18 -0400

    Improvement: error messages now more specific to situation. and improved custom url request.

--- backend/src/services/urlService.js
+++ backend/src/services/urlService.js
@@ -2,9 +2,25 @@ import prisma from "../config/prisma.js";
 import { generateUniqueShortCode } from "../utils/generateShortCode.js";
 
 export const createShortUrl = async (url, custom, userId) => {
-  const shortCode = custom?.trim()
-    ? custom.trim()
+  const trimmedCustom = custom?.trim();
+
+  let customUrlExists;
+
+  if (trimmedCustom) {
+    customUrlExists = await prisma.url.findUnique({
+      where: {
+        shortUrl: trimmedCustom,
+      },
+    });
+  }
+
+  if (trimmedCustom && customUrlExists)
+    throw new Error("The short link already exists");
+
+  const shortCode = trimmedCustom
+    ? trimmedCustom
     : await generateUniqueShortCode();
+
   return await prisma.url.create({
     data: {
       url: url,
