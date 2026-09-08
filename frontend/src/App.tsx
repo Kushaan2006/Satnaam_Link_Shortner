9eb35195931369f6286a06ac59865d44d4287f84
Kushaan2006 <kushaan731@gmail.com>
2026-09-08 18:22:53 -0400

    Implementation: Routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import RouteProtection from "./components/RouteProtection";
+import Analytics from "./pages/Analytics";
 
 export default function App() {
   return (
@@ -8,6 +11,26 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route
+          path="/dashboard"
+          element={
+            <RouteProtection>
+              <Dashboard />
+            </RouteProtection>
+          }
+        />
+        <Route
+          path="/analytics"
+          element={<Navigate to="/dashboard" replace />}
+        />
+        <Route
+          path="/analytics/:id"
+          element={
+            <RouteProtection>
+              <Analytics />
+            </RouteProtection>
+          }
+        />
       </Routes>
     </>
   );
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,5 @@
-import { use, useEffect, useState } from "react";
-import AuthModal from "../components/AuthModal";
 import LogoutButton from "../components/LogoutButton";
-import { useAuth } from "../../hooks/useAuth";
-import { api } from "../api/api";
-import axios from "axios";
-import { redirect } from "react-router-dom";
+
 import LinkShortner from "../components/LinkShortner";
 
 export default function Home() {
