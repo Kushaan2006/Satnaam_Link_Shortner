7f22109a883b2cc68cc0fbfb2ef1505704d79b61
Kushaan2006 <kushaan731@gmail.com>
2026-09-19 18:08:32 -0400

    Added rate limiting for authentication

---
+++ backend/src/middleware/rateLimiter.js
@@ -0,0 +1,51 @@
+import redis from "../config/redis.js";
+
+export const loginLimiter = async (req, res, next) => {
+  try {
+    const ip = req.ip;
+    const key = `rateLimit:login:${ip}`;
+    const count = await redis.incr(key);
+
+    if (count == 1) {
+      await redis.expire(key, 10 * 60);
+    }
+
+    if (count > 5) {
+      console.log(`Login Limiter: ${ip}'s Limit Exceeded`);
+      const ttl = await redis.ttl(key);
+      const minsLeft = ttl < 60 ? "< 1" : Math.ceil(ttl / 60);
+      return res.status(429).json({
+        message: `Too many login attempts. Please try again in ${minsLeft} mins`,
+      });
+    }
+    next();
+  } catch (error) {
+    console.error(`Login Limiter Failure: ${error.message}`);
+    next(); //we'll allow in case redis doesnt work well, its not too strict
+  }
+};
+
+export const signUpLimiter = async (req, res, next) => {
+  try {
+    const ip = req.ip;
+    const key = `rateLimit:signup:${ip}`;
+    const count = await redis.incr(key);
+
+    if (count === 1) {
+      await redis.expire(key, 60 * 60);
+    }
+
+    if (count > 5) {
+      console.log(`Signup Limiter: ${ip}'s limit exceeded`);
+      const ttl = await redis.ttl(key);
+      const minsLeft = ttl < 60 ? "<1" : Math.ceil(ttl / 60);
+      return res.status(429).json({
+        message: `Too many signup attempts. Please try again in ${minsLeft} mins`,
+      });
+    }
+    next();
+  } catch (error) {
+    console.error(`Signup Limiter failure: ${error.message}`);
+    next();
+  }
+};
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -7,11 +7,12 @@ import {
 import { refreshController } from "../controllers/refreshController.js";
 import { checkAuth } from "../middleware/authMiddleware.js";
 import { getMeController } from "../controllers/getMeController.js";
+import { loginLimiter, signUpLimiter } from "../middleware/rateLimiter.js";
 
 const router = express.Router();
 
-router.post("/signup", signUpController);
-router.post("/login", loginController);
+router.post("/signup", signUpLimiter, signUpController);
+router.post("/login", loginLimiter, loginController);
 router.post("/logout", logoutController);
 router.get("/me", checkAuth, getMeController);
 
